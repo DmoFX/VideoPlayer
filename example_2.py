8893daaae47a95a820a7a1f51b86417e0d2e211f
DmoFX <llldemonlll.fun@gmail.com>
2023-03-26 22:48:15 -0700

    Use subprocess to open VLC file.

--- example_2.py
+++ example_2.py
@@ -7,6 +7,7 @@ media_player = vlc.MediaPlayer(video_path)
 
 # start playing video
 media_player.play()
+media_player.vlm_set_loop("1", True)
 
 # wait so the video can be played for 5 seconds
 # irrespective for length of video
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import subprocess
 import os
-# video_path = r"D:/Docs/Work/Python/Projects/Output/ScreenCapture Output/screenshots/video.mp4"
-video_path = "D://Docs//Work//Python//Projects//Output//ScreenCapture Output//screenshots//video.mp4"
+video_path = "D:/Docs/Work/Python/Projects/Output/ScreenCapture Output/screenshots/video.mp4"
+video_path = video_path.replace("/","\\")
+# video_path = "D:\\Docs\\Work\\Python\\Projects\\Output\\ScreenCapture Output\\screenshots\\video.mp4"
 # print(os.path.exists(video_path))
 print(video_path)
-p = subprocess.Popen(["C:/Program Files/VideoLAN/VLC/vlc.exe","D:/video.mp4"])
+# p = subprocess.Popen(["C:/Program Files/VideoLAN/VLC/vlc.exe","D:\\" + "video" + '.mp4'])
+p = subprocess.Popen(["C:/Program Files/VideoLAN/VLC/vlc.exe",video_path,"--loop"])
